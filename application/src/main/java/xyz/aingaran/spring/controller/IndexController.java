51984285f827f7c56f5f0da2dbccb7dde66d6909
AbhiramSandu <38881473+AbhiramSandu@users.noreply.github.com>
2019-03-14 10:30:08 +0530

    Update IndexController.java

--- application/src/main/java/xyz/aingaran/spring/controller/IndexController.java
+++ application/src/main/java/xyz/aingaran/spring/controller/IndexController.java
@@ -1,5 +1,9 @@
 package xyz.aingaran.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @GetMapping
     public String sayHello() {
+        logger.info("Requested to say welcome .");
         return "Hello and Welcome to the DevOps application. You can create a new User by making a POST request to /api/user endpoint.";
     }
 }
